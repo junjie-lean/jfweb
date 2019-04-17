ebb4eb4831362f3562fc6794b382459e678939e5
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2019-04-17 09:26:05 +0800

    多语言解决方案

--- src/config/g.js
+++ src/config/g.js
@@ -2,24 +2,24 @@
  * @Author: junjie.lean
  * @Date: 2019-04-15 15:54:03
  * @Last Modified by: junjie.lean
- * @Last Modified time: 2019-04-16 10:45:20
+ * @Last Modified time: 2019-04-17 09:19:48
  */
 
 /**
  * @description 全局配置
  */
 
+import lang, { systeamTitle } from "./../lang/zh_CN.a.json";
+
 let isDev = process.env.NODE_ENV === "development" ? true : false;
-
-
 const baseConfig = {
-  systeamTitle:"佳发云巅组web端基础框架", /** */
-}
+  systeamTitle /** */
+};
 
 function createGlobalConfig(isDev) {
   let obj = {
-    ...baseConfig,
-  }
+    ...baseConfig
+  };
   return obj;
 }
 
@@ -27,7 +27,6 @@ let g = createGlobalConfig(isDev);
 
 if (window) {
   window.g = g;
-  
 }
 
 export default g;
--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import "./style/index.css";
 import APP from "./view/common components/provider";
 
 ReactDOM.render(<APP />, document.getElementById("root"));
+document.querySelector('head>title').innerHTML = "成都佳发安泰教育科技股份有限公司巅组web前端基础框架"
