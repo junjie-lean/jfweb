35de61c5e0fe38805451bfb3056d01dd3af889df
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2019-07-24 14:42:16 +0800

    0.2 version

--- src/view/pages/home.js
+++ src/view/pages/home.js
@@ -1,46 +1,17 @@
 /*
  * @Author: junjie.lean
- * @Date: 2019-04-17 09:59:08
+ * @Date: 2019-07-24 14:40:12
  * @Last Modified by: junjie.lean
- * @Last Modified time: 2019-05-10 09:26:55
- */
-
-/**
- * @description home index
+ * @Last Modified time: 2019-07-24 14:41:06
  */
 
 import React from "react";
-import lang from "./../../lang/zh_CN";
-import "./../../style/home.scss";
-
-const homelang = lang.home;
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  // static getDerivedStateFromProps(nextProps,prevState){
-  //   return {
-  //     ...prevState
-  //   }
-  // }
-
   render() {
-    let { name, func } = homelang;
     return (
       <>
-        <div className="jfapp-container">
-          <h3>{name ? name : ""}</h3>
-          <ul>
-            {func
-              ? func.map(item => {
-                  return <li key={Math.random()}> {item}</li>;
-                })
-              : ""}
-          </ul>
-        </div>
+        <div>Hello jf-web-app 0.2</div>
       </>
     );
   }
